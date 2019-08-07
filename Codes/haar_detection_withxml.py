be3f2b30c69667d8764cb0ef9b93adcff2775d4f
moojinkim <wj8tp32@gmail.com>
2019-08-07 13:18:32 +0900

    xmls

--- Codes/haar_detection_withxml.py
+++ Codes/haar_detection_withxml.py
@@ -2,10 +2,11 @@ import numpy as np
 import cv2
 import time
 import socket
-moth_cascade = cv2.CascadeClassifier('/Users/moojin/Dropbox/Codes/python/pest_monitoring/Codes/0807_7stage.xml')
+moth_cascade = cv2.CascadeClassifier('whynot.xml')
 ip=socket.gethostbyname(socket.gethostname())
 print(ip)
-img = cv2.imread('/Users/moojin/Dropbox/bugimages/3.jpg')
+img = cv2.imread('5.jpg')
+#img = cv2.imread('C:\Users\wj8tp\Dropbox\bugimages\3.jpg')
 print(len(img))
 print(len(img[0]))
 # img=img[1000:1500,1000:2000]
